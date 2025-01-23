f4029ba92a90e02373b618f1db774c48a2f1d979
ppsegur <pepesegurarodriguez@gmail.com>
2025-01-23 10:40:35 +0100

    Implementación del ejercicio 2 de Python donde miramos si una cadena tienes todas sus llaves bien colocadas con sus respectivos cierres

--- BoletinDeEjercicios/ej2.py
+++ BoletinDeEjercicios/ej2.py
@@ -4,3 +4,30 @@
 #- Paréntesis, llaves y corchetes son igual de prioritarios. No hay uno más importante que otro.
 #- Expresión balanceada: { [ a * ( c + d ) ] - 5 }
 #- Expresión no balanceada: { a * ( c + d ) ] - 5 }
+
+# Función que comprueba si una expresión está balanceada dependiend de sus parentesis , llaves y corchetes
+def esta_balanceada(expresion):
+    # Diccionario de pares de delimitadores
+    pares = {'(': ')', '{': '}', '[': ']'}
+    #Lista vacia para almacenar los delimitadores
+    pila = []
+
+    for caracter in expresion:
+        # Si el caracter es un delimitador de apertura, lo añadimos a la pila
+        if caracter in pares.keys():
+            pila.append(caracter)
+            # Si el caracter es un delimitador de cierre, comprobamos si coincide con el último delimitador de apertura
+        elif caracter in pares.values():
+            if not pila or pares[pila.pop()] != caracter:
+                return False
+
+    # La pila debe estar vacía al final si todo está balanceado
+    return not pila
+
+
+# Ejemplos de uso
+expresion1 = "{ [ a * ( c + d ) ] - 5 }"
+expresion2 = "{ a * ( c + d ) ] - 5 }"
+
+print(esta_balanceada(expresion1))  
+print(esta_balanceada(expresion2)) 
