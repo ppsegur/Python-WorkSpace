b42adf929920b2db3d0e9cc218cf1d4274ac9dba
ppsegur <pepesegurarodriguez@gmail.com>
2025-02-06 12:11:16 +0100

    Ejercicio 5 de python realizaso

---
+++ BoletinDeEjerciciosIntermedios2/Ejercicio5.py
@@ -0,0 +1,8 @@
+# Obtén el índice y el valor como una tupla para los elementos de la lista 
+# “hi”, 4, 8.99, 'apple', ('t,b','n'). 
+# El resultado se vería así (índice, valor), (índice, valor)
+lista = ["hi", 4, 8.99, 'apple', ('t,b','n')]
+#Creamos una lista de tuplas con los indices y valores de la lista
+Tuplas = [(i, lista[i]) for i in range(len(lista))]
+print(Tuplas)
+
