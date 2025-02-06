43151701ca38983876dafc8d25dc6085c1c21e5b
ppsegur <pepesegurarodriguez@gmail.com>
2025-02-06 12:22:20 +0100

    Creación del ejercicio 7 de python

---
+++ BoletinDeEjerciciosIntermedios2/Ejercicio7.py
@@ -0,0 +1,7 @@
+
+
+lista = [ 'En', '1984', 'hubo', '13' ,'casos',  'de', 'protesta ','con' 'más', 'de' ,'1000' ,'asistentes']
+
+listaNumeros = [palabra for palabra in lista if palabra.isnumeric()]
+
+print(listaNumeros)
